b2159d0be725e661a4d9790b8c7bb27cbad7e594
Kanhaiya lal sharma <kanhaiyalalsharma0409@gmail.com>
2025-08-06 20:19:15 +0530

    Update cors in server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://auto-mailerx.vercel.app", 
+}));
 
 app.post("/send-mail", async (req, res) => {
   const { to,Name} = req.body;
