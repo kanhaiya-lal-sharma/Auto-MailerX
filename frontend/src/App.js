45bce79f568512682645df3b30904161d654a44b
Kanhaiya lal sharma <kanhaiyalalsharma0409@gmail.com>
2025-08-06 21:22:50 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://auto-mailerx.onrender.com/send-email", {
+      const res = await fetch("https://auto-mailerx.onrender.com/send-mail", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
